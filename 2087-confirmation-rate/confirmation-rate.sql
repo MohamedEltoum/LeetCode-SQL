e49f179c204227b2ccf06ac5b4c8e4c51f3dc063
MohamedEltoum <157864944+MohamedEltoum@users.noreply.github.com>
2024-01-29 18:00:56 +0200

    Time: 2630 ms (5.03%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT s.user_id, ISNULL(ROUND(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) * 1.0 / NULLIF(COUNT(c.action), 0), 2), 0) confirmation_rate
+SELECT s.user_id, ISNULL(ROUND(SUM(IIF(c.action = 'confirmed', 1, 0)) * 1.0 / NULLIF(COUNT(c.action), 0), 2), 0) confirmation_rate
 FROM Signups s LEFT JOIN Confirmations c
 ON s.user_id = c.user_id
 GROUP BY s.user_id;
