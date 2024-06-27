155008e8689fc6b71d8e9624084e6cc49a8e42a4
MohamedEltoum <157864944+MohamedEltoum@users.noreply.github.com>
2024-06-27 22:44:40 +0200

    Time: 655 ms (87.14%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,9 +1,7 @@
 /* Write your T-SQL query statement below */
-SELECT name, travelled_distance
-FROM
-    (SELECT u.id, u.name, ISNULL(SUM(r.distance), 0) AS travelled_distance
-    FROM Users u
-        LEFT JOIN Rides r
-        ON u.id = r.user_id
-    GROUP BY u.id, u.name) AS Join_Table
+SELECT u.name, ISNULL(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY u.id, u.name
 ORDER BY travelled_distance DESC, name ASC;
