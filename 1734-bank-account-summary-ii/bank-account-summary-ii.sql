84f1ce2073c4955a88e230592cad4fe14d079871
MohamedEltoum <157864944+MohamedEltoum@users.noreply.github.com>
2024-06-27 22:50:26 +0200

    Time: 460 ms (76.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT u.name, SUM(t.amount) AS balance
+FROM Users u
+    LEFT JOIN Transactions t
+    ON u.account = t.account
+GROUP BY u.name, u.account
+HAVING SUM(t.amount) > 10000;
